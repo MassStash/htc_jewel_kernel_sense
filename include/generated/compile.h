9aebc9d6bcfb369cf3a9f0c55190d8a1d7834564
MassStash <MassStash@gmail.com>
2014-07-27 12:04:05 -0500

    Big update to how things cook 'round here
    Signed-off-by: MassStash <MassStash@gmail.com>

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 0 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#0 SMP PREEMPT Thu Jul 24 19:20:30 CDT 2014"
+#define UTS_VERSION "#0 SMP PREEMPT Sat Jul 26 19:34:11 CDT 2014"
 #define LINUX_COMPILE_BY "zaichron-lin"
 #define LINUX_COMPILE_HOST "zaichron-lin-pc"
-#define LINUX_COMPILER "gcc version 4.9.1 20140710 (prerelease) (crosstool-NG linaro-1.13.1+bzr2673 - Linaro GCC 2014.07 - Cortex-A15) "
+#define LINUX_COMPILER "gcc version 4.9.1 20140710 (prerelease) (crosstool-NG linaro-1.13.1+bzr2673 - Linaro GCC 2014.07 - Cortex-A8) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.10-gdd2d6c5"
+#define UTS_RELEASE "3.4.10-MassStashedX-0.0.1-beta-g86c5a91-dirty"
